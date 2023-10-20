0c9e35a400678ec3f21bbd1e01102e430507df82
Taïder Melvin <tmneo1989@hotmail.com>
2023-10-20 12:42:13 +0200

    For_OK

--- demo08_boucles.py
+++ demo08_boucles.py
@@ -3,8 +3,9 @@
 # "while" & "for"
 # Elles permettent:
 # - de continuer de faire tourner un programme tant que la condition est vraie,
-# jusqu'à une limite définie ou jusqu'à ce que l'utilisateur décide de l'arrêter.
+#   jusqu'à une limite définie ou jusqu'à ce que l'utilisateur décide de l'arrêter.
 # - d'afficher les éléments d'une collection.
+
 
 
 # LA BOUCLE "while"
@@ -25,6 +26,7 @@ print()
 # On créera une boucle INFINIE !!!
 # Ce qui peut être plutôt dangereux pour la machine!
 
+
 # Le "break"
 # Permet de stopper la boucle, même si la condition est toujours vraie.
 i = 1
@@ -35,6 +37,7 @@ while i <= 5:
     i += 1
 print()
 
+
 # Le "continue"
 # Permet de stopper l'itéraction actuelle et de passer à la suivante.
 i = 0
@@ -44,6 +47,7 @@ while i <= 5:
         continue
     print(i)
 print()
+
 
 # Le "else"
 # Permet d'exécuter un dernier bloc de code une fois la boucle terminée.
@@ -57,9 +61,10 @@ else:
 print()
 
 
+
 # LA BOUCLE "for"
 
-# Pour parcourir et afficher tous les éléments d'une liste,
+# Parcourir et afficher tous les éléments d'une liste,
 # il va falloir créer une boucle (for) qui va lire chaque élément, pour ensuite l'afficher.
 myList = ["Pomme", 1, True, "Pomme", 4, "Poire"]
 for element in myList:
@@ -67,11 +72,89 @@ for element in myList:
 # On dit au programme,
 # "pour" chaque "élément" "dans" "ma liste": "affiche"("l'élément")
 
-# On peut également faire tout cela via les index,
-# avec pour "range" la "longueur" de la liste.
-# Habituellement on utilise "i" pour les itérations.
+# Parcourir une chaîne de caractères.
+for char in myList[0]:
+    print(char)
+print()
+
+
+# Le "break"
+for element in myList:
+    print(element)
+    if element == 4:
+        break
+print()
+# Si on met le "print(element)" après la condition:
+for element in myList:
+    if element == 4:
+        break
+    print(element)
+print()
+
+
+# Le "continue"
+for element in myList:
+    if element == 4:
+        continue
+    print(element)
+print()
+
+
+# La fonction "range()"
+# Elle retourne une séquence de nombres,
+# commençant par 0 par défaut,
+# s'incrémentant de 1 par défaut,
+# et fini à un nombre spécifié.
+for i in range(10):
+    print(i)
+print()
+# On remarque ici qu'on obtient bien 6 valeurs, en commençant par 0!
+# Le 6 est donc exclusif!
+
+# Et donc dans une liste via les index...
 for i in range(len(myList)):
-    print() # un simple saut de ligne pour la clareté.
+    print()
     print(i) # en faisant cela, nous affichons l'index des éléments!
     print(myList[i]) # ici nous affichons bien l'élément de la liste, récupéré à l'index!
+print()
 
+# Si le premier paramètre donné est la limite,
+# On peut en rajouter un pour modifier la valeur par défaut de commencement.
+for i in range(2, 10):
+    print(i)
+print()
+# La valeur de début est inclusive!
+
+# On peut en rajouter encore un autre pour modifier la valeur par défaut de l'incrément.
+for i in range(2, 10, 2):
+    print(i)
+print()
+
+# Le "else"
+for i in range(10):
+    print(i)
+else:
+    print("Boucle terminée!")
+print()
+
+# *** ATTENTION ***
+# Si un "break" se trouve dans la boucle,
+# le bloc "else" ne s'exécutera pas !!!
+for i in range(10):
+    print(i)
+    if i == 5: break
+else:
+    print("Boucle terminée!")
+print()
+
+
+# Les boucles imbriquées (Nested For Loops)
+integers = [1, 2, 3, 4, 5]
+strings = ["un", "deux", "trois", "quatre", "cinq"]
+
+for num in integers:
+    print("Première boucle à l'élément :", num)
+    for string in strings:
+        print("Seconde boucle...", string)
+    print()
+# Ce concept d'imbrication existe aussi avec les conditions (Nested If)!
